3144cdd3cc292f1158d139ac70feb267ee78c2f5
Alexis Schwindt <alexis.schwindt@outlook.com>
2023-11-24 20:46:00 -0300

    Added comments for the love of comments. Also added early returns.

--- MyFirstProgram/main.c
+++ MyFirstProgram/main.c
@@ -1,6 +1,10 @@
 #include "test.h"
 
 int main(const int argc, char** argv) {
+    /*
+     * We iterate over every argument except the first one and print them calling the function with the argv as argument.
+     * We check for error codes and do an early return if something fails, or keep going until the 0 code.
+    */
     for (int i = 1; i < argc; ++i) {
         if (test_PrintMyName(argv[i]) != 0) return 1;
     }
--- MyFirstProgram/test.c
+++ MyFirstProgram/test.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 
 int test_PrintMyName(char name[]) {
+    // Early return in case of null arguments with error code 1
+    if (!name) return 1;
+
     printf("Hello, my name is %s", name);
     return 0;
 }
