f8c6809214cbf6bef2b0e888df32b0bca4b0ff39
Alexis Schwindt <alexis.schwindt@outlook.com>
2023-11-24 20:32:59 -0300

    Modified the logic to avoid printing the path of the app.

--- MyFirstProgram/main.c
+++ MyFirstProgram/main.c
@@ -1,7 +1,7 @@
 #include "test.h"
 
 int main(const int argc, char** argv) {
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 1; i < argc; ++i) {
         if (test_PrintMyName(argv[i]) != 0) return 1;
     }
     return 0;
